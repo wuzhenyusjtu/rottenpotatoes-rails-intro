6f56488f61d8d850bfa04fa16c132ef53f2283a4
EC2 Default User <ec2-user@ip-172-31-23-251.us-east-2.compute.internal>
2020-09-16 05:18:05 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,8 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.ratings
     @sort = params[:sort]
-    @movies = Movie.all.order(@sort)
+    params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    @movies = Movie.where(rating: @t_param).order(@sort)
   end
 
   def new
